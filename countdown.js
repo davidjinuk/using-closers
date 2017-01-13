4d8fd8c54c72a65ba6e2c6978717d51dc792b883
David Kim <davidjinuk90@gmail.com>
2017-01-13 03:00:43 +0000

    Used the previous function to create the code

---
+++ countdown.js
@@ -0,0 +1,24 @@
+var countdownGenerator = function (x) {
+  /* your code here */
+  var output = ['T-minus', 'Blast Off!', 'Rockets alreadygone, bub!'];
+  var count = x + 1;
+
+  return function () {
+    count -= 1;
+    if (count > 0) {
+      return console.log(output[0] + ' ' + count + '...');
+    }
+    if (count = 0) {
+      return console.log(output[1]);
+    }
+    return console.log(output[2]);
+  }
+};
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
